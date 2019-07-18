ba2501999ab8ff98fedbf3bfb5347be81389f1ae
Achudh Krishna <achudhk@gmail.com>
2019-07-18 11:06:40 +0530

    init commit added firebase and moved app

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAz4mfpp-DU3u2qs-7-k71xoKzMG8bAtzw",
+    authDomain: "iiamwcf.firebaseapp.com",
+    databaseURL: "https://iiamwcf.firebaseio.com",
+    projectId: "iiamwcf",
+    storageBucket: "",
+    messagingSenderId: "676744947002",
+    appId: "1:676744947002:web:f0ac6feb8bbc83da"
+  };
